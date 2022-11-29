761e8bb50e2042ccd515766740759b7a751f0594
Lawless Hollis <hollis.lawrence.moore@gmail.com>
2022-11-28 19:56:05 -0600

    Exercise completed!

--- DOM-and-Events-Studio-master/scripts.js
+++ DOM-and-Events-Studio-master/scripts.js
@@ -2,9 +2,15 @@
 // Remember to pay attention to page loading!
 window.addEventListener('load', function() {
 
+    let rocketPosX = 0;
+    let rocketPosY = 0;
+    let altitude = 0;
+
     const takeOffButton = document.getElementById('takeoff');
     const landButton = document.getElementById('landing');
     const abortButton = document.getElementById('missionAbort');
+
+    const rocket = document.getElementById('rocket');
 
     const flightStatus = document.getElementById('flightStatus');
     const shuttleBackground = document.getElementById('shuttleBackground');
@@ -17,6 +23,8 @@ window.addEventListener('load', function() {
         flightStatus.innerHTML = 'Shuttle in flight';
         shuttleBackground.style.backgroundColor = 'blue';
         spaceShuttleHeight.innerHTML = 10000;
+        rocketPosY = 10;
+        rocket.style.marginBottom = rocketPosY + 'px';
        }
 
     });
@@ -26,6 +34,7 @@ window.addEventListener('load', function() {
          flightStatus.innerHTML = 'Shuttle has landed';
          shuttleBackground.style.backgroundColor = 'green';
          spaceShuttleHeight.innerHTML = 0;
+         resetRocket();
      });
 
     abortButton.addEventListener('click', function() {
@@ -35,8 +44,41 @@ window.addEventListener('load', function() {
          flightStatus.innerHTML = 'MISSION ABORTED';
          shuttleBackground.style.backgroundColor = 'green';
          spaceShuttleHeight.innerHTML = 0;
+         resetRocket();
         }
  
      });
+
+     this.document.addEventListener('click', function(event) {
+        let bkgWidth = parseInt(window.getComputedStyle(shuttleBackground).getPropertyValue('width'));
+
+        if (event.target.id === 'left' && rocketPosX > -(bkgWidth / 2 - 40)) {
+            rocketPosX -= 10;
+            rocket.style.marginLeft = rocketPosX + 'px';
+        }
+        if (event.target.id === 'right' && rocketPosX < (bkgWidth / 2 - 40)) {
+            rocketPosX += 10;
+         rocket.style.marginLeft = rocketPosX + 'px';
+        }
+        if (event.target.id === 'up' && altitude < 250000) {
+            rocketPosY += 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude += 10000;
+            spaceShuttleHeight.innerHTML = altitude;
+        }
+        if (event.target.id === 'down' && altitude > 0) {
+            rocketPosY -= 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude -= 10000;
+            spaceShuttleHeight.innerHTML = altitude;
+    }
+     })
+
+     function resetRocket() {
+        rocketPosX = 0;
+        rocketPosY = 0;
+        rocket.style.marginLeft = rocketPosX + 'px';
+        rocket.style.marginBottom = rocketPosY + 'px';
+      }
 
 });
