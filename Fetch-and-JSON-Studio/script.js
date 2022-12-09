f4732533c2f745e812bbc34e7f0ddf5d52b14bc4
Lawless Hollis <hollis.lawrence.moore@gmail.com>
2022-12-08 21:08:23 -0600

    final changes complete, studio finished

--- Fetch-and-JSON-Studio/script.js
+++ Fetch-and-JSON-Studio/script.js
@@ -2,24 +2,32 @@
 window.addEventListener('load', function() {
 
     const container = document.getElementById("container");
+    const count = document.getElementById("count"); 
 
     this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(data) {
             console.log(data);
+
+            count.innerHTML = `There are ${data.length} astronauts! Wow! That's more than ${data.length - 1}!`;
+
+            data.sort(function(a, b) {
+                return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+            });
+
             for (let i = 0; i < data.length; i++) {
             let astronaut = data[i];
             container.innerHTML += `
-            <div class="astronaut">
-            <div class="bio">
-                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
-                <ul>
-                <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                <li>Active: ${astronaut.active}</li>
-                <li>Skills: ${astronaut.skills.join(", ")}</li>
-                </ul>
-                 </div>
-                 <img class="avatar" src="${astronaut.picture}">
-            </div>`
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li class="${astronaut.active ? "green" : ""}">Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${astronaut.picture}">
+                </div>`
             }
 
         });
